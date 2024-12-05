6fda16ffc568c779b956c58718300d61187a2b12
NeroCorleone <nefta.kanilmaz@gmail.com>
2024-12-05 14:35:57 +0000

    Fix usage of emoji

--- R/hello.R
+++ R/hello.R
@@ -9,7 +9,10 @@
 #' @examples
 #' in Python: print("Hello World")
 #'
+#' @import emoji
+#' @export hello
+
 
 hello = function(name = "world") {
-  paste0("Hello ", name, "! Welcome to unkr!")
+ paste0("Hello ", name, "! Welcome to unkr!", emoji::emoji("wave"))
 }
---
+++ tests/testthat/test-hello.R
@@ -0,0 +1,4 @@
+test_that("output is character", {
+  expect_type(hello(), "character")
+})
+
